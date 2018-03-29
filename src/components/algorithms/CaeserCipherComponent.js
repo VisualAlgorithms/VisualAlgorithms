8fbeaec195d249a22937d458de57c208798200c8
lightning0 <zbsmith777@gmail.com>
2018-03-29 16:32:50 -0500

    Tryed to implamnt the CaeserCipherComponent.js
    @mrwatts88 Would you take a look at this?

---
+++ src/components/algorithms/CaeserCipherComponent.js
@@ -0,0 +1,25 @@
+import '../../css/caesarcipher.css';
+import React from 'react';
+import Navbar from '../Navbar';
+import { Router, Route, Switch } from 'react-router';
+//import { LinkedList } from '../../../datastructures/linkedlist/linkedlist';
+import P5Wrapper from 'react-p5-wrapper'
+
+export default class CaesarCipherComponent extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {
+        return (<div>
+            <Navbar />
+            <div className="container">
+                <h1 className="display-3">Hello World!</h1>
+                <p>This website is dedicated to those who ask questions and seek solutions to some of the worlds most difficult problems.
+            We hope you enjoy the journey as much as we do. ~Z</p>
+                <a className="btn btn-primary btn-lg" href="./html/about.html" role="button">Learn more »</a>
+            </div>
+        </div>
+        );
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import App from './components/App';
 import LinkedListComponent from './components/datastructures/LinkedListComponent';
+import CaeserCipherComponent from './components/algorithms/CaeserCipherComponent';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
@@ -7,8 +8,8 @@ const Routes = () => (
     <Switch>
         <Route exact path='/' component={App} />
         <Route exact path='/linkedlist' component={LinkedListComponent} />
-        {/* <Route exact path='/caesercipher' component={LinkedListComponent} />
-        <Route exact path='/test' component={LinkedListComponent} /> */}
+        <Route exact path='/caesercipher' component={CaeserCipherComponent} />
+        {/* <Route exact path='/test' component={LinkedListComponent} /> */}
     </Switch>
 );
 
