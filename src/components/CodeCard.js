26560189d52051bdffb242d5ef3356406ce4e567
lightning0 <zbsmith777@gmail.com>
2018-03-29 21:04:39 -0500

    Updated code Card and CaeserCipher

--- src/components/CodeCard.js
+++ src/components/CodeCard.js
@@ -9,14 +9,14 @@ export default class CodeCard extends React.Component {
 
     render() {
         return (<div>
-            <div class="card" style="width: 18rem;">
-                {/* <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div> */}
+            <div className="card" style={{width: '18rem'}}>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                </div>
             </div>
         </div>
         );
--- src/components/algorithms/CaeserCipherComponent.js
+++ src/components/algorithms/CaeserCipherComponent.js
@@ -13,8 +13,8 @@ export default class CaesarCipherComponent extends React.Component {
     render() {
         return (<div>
             <Navbar />
-            <div class="container">
-                <h1 class="display-3">Welcome Fellow Romans!</h1>
+            <div className="container">
+                <h1 className="display-3">Welcome Fellow Romans!</h1>
                 <p>
                     In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one
                     of the simplest and most widely known encryption techniques. It is a type of substitution cipher
@@ -23,9 +23,9 @@ export default class CaesarCipherComponent extends React.Component {
                     so on. The method is named after Julius Caesar, who used it in his private correspondence.
                         <br />
                 </p>
-                <div class="float-right">~Wikipedia</div>
+                <div className="float-right">~Wikipedia</div>
                 <p>
-                    <a class="btn btn-lg" href=".https://en.wikipedia.org/wiki/Caesar_cipher" role="button">Learn more »</a>
+                    <a className="btn btn-lg" href=".https://en.wikipedia.org/wiki/Caesar_cipher" role="button">Learn more »</a>
                 </p>
                 <CodeCard />
                 <CodeCard />
