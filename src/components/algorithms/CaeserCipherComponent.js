2765cb52fdad4cf280ef1cd004af23a47583852a
Matt <matthew.watts.mw@gmail.com>
2018-03-30 00:52:17 -0500

    u

--- src/components/algorithms/CaeserCipherComponent.js
+++ src/components/algorithms/CaeserCipherComponent.js
@@ -24,7 +24,6 @@ export default class CaesarCipherComponent extends React.Component {
             <Navbar />
             <div className="container">
                 <h1 className="display-3">Hello everybody!</h1>
-
                 <p>This website is dedicated to those who ask questions and seek solutions to some of the worlds most difficult problems.
             We hope you enjoy the journey as much as we do. ~Z</p>
                 <a className="btn btn-primary btn-lg" href="./html/about.html" role="button">Learn more »</a>
