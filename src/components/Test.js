f076791705441703b70c80621ce719ade7ed7251
lightning0 <zbsmith777@gmail.com>
2018-04-07 10:26:28 -0500

    Add navbar to Test
    I added the navbar but somhow I broke the button Hiding.
    
    I had to add custom css to get the Mocha Stats to show.

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import tests from '../../test/linkedlist.test';
+import Navbar from "./Navbar";
+import testCSS from "../css/Test.css"
+
 export default class Test extends React.Component {
     constructor() {
         super();
         this.test = this.test.bind(this);
+
+        const hideDiv = {
+            display: 'none',
+        };
+
     }
 
     test() {
@@ -12,11 +20,20 @@ export default class Test extends React.Component {
         mocha.run();
     }
 
+    hide() {
+        // this.style.display="none"
+        // return style = { hideDiv };
+        // return <div style={divStyle}>Hello World!</div>;
+    }
+
     render() {
         return (
             <div>
-                <button className="btn btn-secondary" onClick={this.test}>Run Tests</button>
-                <div id="mocha" ></div >
+                <Navbar />
+                <div onClick={this.hide}>
+                    <button className="btn btn-secondary " onClick={this.test}>Run Tests</button>
+                </div>
+                <div className="testCSS" id="mocha" ></div >
             </div>
         );
     }
